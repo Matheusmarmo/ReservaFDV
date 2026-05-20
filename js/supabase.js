5622d7e2de06431d697abd2d968f65064153276c
Matheusmarmo <103460748+Matheusmarmo@users.noreply.github.com>
2026-05-20 00:29:02 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -5,6 +5,6 @@
 // ============================================================
 
 const SUPABASE_URL      = 'https://wfniapkstveovsnbjolw.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_cJcJk_eDUzLjgNWVahX3jQ_C0U3Ub7r';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmbmlhcGtzdHZlb3ZzbmJqb2x3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MjM3MzksImV4cCI6MjA5NDI5OTczOX0.SlHGn28rQ6buW_h16HM0bnHBRuq9Ulls1S71dnLlXnc';
 
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
